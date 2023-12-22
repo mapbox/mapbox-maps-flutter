514a1e184c0b33eca8df5fa47e928f8fa23fcc5c
Roman Laitarenko <roman.laitarenko@mapbox.com>
2023-12-22 10:57:41 +0200

    Add the ability to subscribe to onMapLoaded in integration tests

--- example/lib/empty_map_widget.dart
+++ example/lib/empty_map_widget.dart
@@ -1,17 +1,26 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+final onMapLoaded = Completer();
 
 Future<MapboxMap> main() {
   final completer = Completer<MapboxMap>();
 
   runApp(MaterialApp(
       home: MapWidget(
-          key: ValueKey("mapWidget"),
-          onMapCreated: (MapboxMap mapboxMap) {
-            completer.complete(mapboxMap);
-          })));
+    key: ValueKey("mapWidget"),
+    onMapCreated: (MapboxMap mapboxMap) {
+      completer.complete(mapboxMap);
+    },
+    onMapLoadedListener: (MapLoadedEventData data) {
+      if (!onMapLoaded.isCompleted) {
+        onMapLoaded.complete();
+      }
+    },
+  )));
 
   return completer.future;
 }
