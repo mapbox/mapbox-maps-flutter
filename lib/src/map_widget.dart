c4094adf7bd78971f69908d417db79dcf4d73b5a
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-02-02 20:42:09 +0200

    Bump version to 1.0.0-beta.3

--- lib/src/map_widget.dart
+++ lib/src/map_widget.dart
@@ -217,7 +217,7 @@ class _MapWidgetState extends State<MapWidget> {
       'textureView': widget.textureView,
       'styleUri': widget.styleUri,
       'eventTypes': widget._eventTypes.map((e) => e.index).toList(),
-      'mapboxPluginVersion': '1.0.0-beta.2'
+      'mapboxPluginVersion': '1.0.0-beta.3'
     };
 
     return _mapboxMapsPlatform.buildView(widget.androidHostingMode,
