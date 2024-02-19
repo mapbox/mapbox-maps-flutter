230d9967d14c91e493187b4f6f28cb24d60b79da
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-02-19 14:31:03 +0200

    Rename iOS's MapboxMapsPlugin

--- ios/Classes/MapboxMapsPlugin.swift
+++ ios/Classes/MapboxMapsPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-public class SwiftMapboxMapsPlugin: NSObject, FlutterPlugin {
+public class MapboxMapsPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = MapboxMapFactory(withRegistrar: registrar)
         registrar.register(instance, withId: "plugins.flutter.io/mapbox_maps")
