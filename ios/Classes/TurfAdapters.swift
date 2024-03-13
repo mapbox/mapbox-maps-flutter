557490a78bb8c174a2d7a11646d818cff092a5af
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-03-13 15:21:39 +0200

    lint

--- ios/Classes/TurfAdapters.swift
+++ ios/Classes/TurfAdapters.swift
@@ -3,8 +3,11 @@ import Turf
 
 extension Point {
     static func fromList(_ list: [Any?]) -> Point! {
+        // swiftlint:disable:next force_cast
         let rawPoint = list.first as! [String: Any]
+        // swiftlint:disable:next force_cast
         let coordinates = rawPoint["coordinates"] as! [Double]
+
         return Point(
             LocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
         )
