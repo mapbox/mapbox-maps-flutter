902d0c83c92fa633e3e1852c871e56cfe420d226
Mai Mai <mai.mai@mapbox.com>
2024-04-26 13:58:19 +0300

    Prepare v0.6.0 release (#510)
    * bump deps version to v10.17.0 (iOS & Android)
    
    * bump Maps Flutter to v0.6.0
    
    * Fix StyleColor -> Int conversion

--- lib/src/map_widget.dart
+++ lib/src/map_widget.dart
@@ -188,7 +188,7 @@ class _MapWidgetState extends State<MapWidget> {
       'textureView': widget.textureView,
       'styleUri': widget.styleUri,
       'eventTypes': widget._eventTypes,
-      'mapboxPluginVersion': '0.5.1'
+      'mapboxPluginVersion': '0.6.0'
     };
 
     return _mapboxMapsPlatform.buildView(
