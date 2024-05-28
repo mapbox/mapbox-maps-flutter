d0610cd6d52bc87cce1f4aa47b2d9c992445cc14
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-05-28 15:05:15 +0300

    Add optional casting helpers

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,4 +2,22 @@ part of mapbox_maps_flutter;
 
 /// Returns back passed value casted to the desired type
 /// or null if typecasting fails
-T? optionalCast<T>(dynamic value) => value is T ? value : null;
+T? _optionalCast<T>(dynamic value) {
+  if (value is T) {
+    return value;
+  }
+  // Cast int(num) to double,
+  // as GL Native converts e.g. 1.0 to 1 (int),
+  // which trips dart up
+  if (value is num && T == double) {
+    return value.toDouble() as T;
+  }
+  return null;
+}
+
+List<T>? _optionalCastList<T>(dynamic value) {
+  if (value is List) {
+    return value.where((value) => value is T).cast<T>().toList();
+  }
+  return null;
+}
