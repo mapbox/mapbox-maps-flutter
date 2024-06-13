a8227e0b7e052b54f5bde8f9a4983b01665d981c
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-06-13 09:39:59 +0300

    Update lib/src/utils.dart
    Co-authored-by: Patrick Leonard <pjleonard37@users.noreply.github.com>

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,6 +1,6 @@
 part of mapbox_maps_flutter;
 
-/// Returns back passed value casted to the desired type
+/// Returns back the passed value casted to the desired type
 /// or null if typecasting fails
 T? _optionalCast<T>(dynamic value) {
   if (value is T) {
