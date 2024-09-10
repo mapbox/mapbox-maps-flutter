c3be1613b1d052501d58f1786cd91fb203116fee
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-09-10 15:47:07 +0300

    [debug]

--- example/lib/empty_map_widget.dart
+++ example/lib/empty_map_widget.dart
@@ -25,31 +25,41 @@ Future<MapboxMap> main() {
   final completer = Completer<MapboxMap>();
 
   const ACCESS_TOKEN = String.fromEnvironment('ACCESS_TOKEN');
-  MapboxOptions.setAccessToken(ACCESS_TOKEN);
-
+  MapboxOptions.setAccessToken(
+      "pk.eyJ1IjoiZXZpbDE1OSIsImEiOiJja3licnp3aW0waWU1MnZuMGFpcXgzMTN5In0.34sUmpVbGc4VEWWXxvWelA");
+  print("kkk: running the app");
   events = Events();
   runApp(MaterialApp(
       home: MapWidget(
     key: ValueKey("mapWidget"),
     onMapCreated: (MapboxMap mapboxMap) {
+      print("kkk: map created");
       completer.complete(mapboxMap);
     },
     onMapLoadedListener: (MapLoadedEventData data) {
+      print("kkk: map loaded");
       if (!events.onMapLoaded.isCompleted) {
         events.onMapLoaded.complete();
+      } else {
+        throw Exception();
       }
     },
     onStyleLoadedListener: (StyleLoadedEventData data) {
+      print("kkk: style loaded");
       if (!events.onStyleLoaded.isCompleted) {
         events.onStyleLoaded.complete();
+      } else {
+        throw Exception();
       }
     },
     onStyleDataLoadedListener: (StyleDataLoadedEventData data) {
+      print("kkk: style data loaded");
       if (!events.onStyleDataLoaded.isCompleted) {
         events.onStyleDataLoaded.complete();
       }
     },
     onSourceDataLoadedListener: (SourceDataLoadedEventData data) {
+      print("kkk: source data loaded");
       if (!events.onSourceDataLoaded.isCompleted) {
         events.onSourceDataLoaded.complete();
       }
@@ -60,6 +70,7 @@ Future<MapboxMap> main() {
       }
     },
     onMapIdleListener: (MapIdleEventData data) {
+      print("kkk: map idle");
       if (!events.onMapIdle.isCompleted) {
         events.onMapIdle.complete();
       }
@@ -76,7 +87,8 @@ Future<MapboxMap> runFixedSizeMap() {
   final completer = Completer<MapboxMap>();
 
   const ACCESS_TOKEN = String.fromEnvironment('ACCESS_TOKEN');
-  MapboxOptions.setAccessToken(ACCESS_TOKEN);
+  MapboxOptions.setAccessToken(
+      "pk.eyJ1IjoiZXZpbDE1OSIsImEiOiJja3licnp3aW0waWU1MnZuMGFpcXgzMTN5In0.34sUmpVbGc4VEWWXxvWelA");
 
   runApp(MaterialApp(
       home: Align(
@@ -99,7 +111,8 @@ Future<MapboxMap> runMapWithCustomCamera(CameraOptions camera) {
   final completer = Completer<MapboxMap>();
 
   const ACCESS_TOKEN = String.fromEnvironment('ACCESS_TOKEN');
-  MapboxOptions.setAccessToken(ACCESS_TOKEN);
+  MapboxOptions.setAccessToken(
+      "pk.eyJ1IjoiZXZpbDE1OSIsImEiOiJja3licnp3aW0waWU1MnZuMGFpcXgzMTN5In0.34sUmpVbGc4VEWWXxvWelA");
 
   runApp(MaterialApp(
     home: MapWidget(
@@ -115,7 +128,8 @@ Future<MapboxMap> runMapWithCustomCamera(CameraOptions camera) {
 
 void runEmpty() {
   const ACCESS_TOKEN = String.fromEnvironment('ACCESS_TOKEN');
-  MapboxOptions.setAccessToken(ACCESS_TOKEN);
+  MapboxOptions.setAccessToken(
+      'pk.eyJ1IjoiZXZpbDE1OSIsImEiOiJja3licnp3aW0waWU1MnZuMGFpcXgzMTN5In0.34sUmpVbGc4VEWWXxvWelA');
 
   runApp(MaterialApp());
 }
