26a85af292f1ee5e960d8a2d90ff843232a3a34d
Patrick Leonard <pjleonard37@users.noreply.github.com>
2025-02-14 06:38:01 -0500

    Update Standard Style Interactions Examples, Fix Bug (#865)
    * Update examples
    
    * Group newer examples together
    
    ---------
    
    Co-authored-by: Release SDK bot for Maps SDK team <maps_sdk_ios@mapbox.com>
    Co-authored-by: Roman Laitarenko <roman.laitarenko@mapbox.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,8 @@ import 'package:mapbox_maps_example/ornaments_example.dart';
 import 'package:mapbox_maps_example/geojson_line_example.dart';
 import 'package:mapbox_maps_example/image_source_example.dart';
 import 'package:mapbox_maps_example/map_interface_example.dart';
-import 'package:mapbox_maps_example/interactive_features_example.dart';
+import 'package:mapbox_maps_example/standard_style_import_example.dart';
+import 'package:mapbox_maps_example/standard_style_interactions_example.dart';
 import 'package:mapbox_maps_example/polygon_annotations_example.dart';
 import 'package:mapbox_maps_example/polyline_annotations_example.dart';
 import 'package:mapbox_maps_example/simple_map_example.dart';
@@ -40,9 +41,10 @@ final List<Example> _allPages = <Example>[
   ModelLayerExample(),
   DebugOptionsExample(),
   SpinningGlobeExample(),
+  StandardStyleImportExample(),
+  StandardStyleInteractionsExample(),
   FullMapExample(),
   StyleExample(),
-  InteractiveFeaturesExample(),
   CameraExample(),
   ProjectionExample(),
   MapInterfaceExample(),
