3df992694b0b0669ab0f23f7c06d21b30cff8a3f
lobuli <leshek.studios@gmail.com>
2025-03-03 13:19:01 -0700

    feat: add traffic layer (#792)
    Co-authored-by: Patrick Leonard <pjleonard37@users.noreply.github.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,6 +16,7 @@ import 'package:mapbox_maps_example/polygon_annotations_example.dart';
 import 'package:mapbox_maps_example/polyline_annotations_example.dart';
 import 'package:mapbox_maps_example/simple_map_example.dart';
 import 'package:mapbox_maps_example/snapshotter_example.dart';
+import 'package:mapbox_maps_example/traffic_layer_example.dart';
 import 'package:mapbox_maps_example/spinning_globe_example.dart';
 import 'package:mapbox_maps_example/traffic_route_line_example.dart';
 import 'package:mapbox_maps_example/tile_json_example.dart';
@@ -62,6 +63,7 @@ final List<Example> _allPages = <Example>[
   GesturesExample(),
   OrnamentsExample(),
   AnimatedRouteExample(),
+  TrafficLayerExample(),
 ];
 
 class MapsDemo extends StatelessWidget {
