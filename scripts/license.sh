609f06e48989f933a84b45b33e59c4e7bb568cef
Mai Mai <mai.mai@mapbox.com>
2025-05-16 15:54:58 +0300

    fix license script

--- scripts/license.sh
+++ scripts/license.sh
@@ -3,45 +3,55 @@ set -eou pipefail
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 --mode <validate|generate>"
+    echo "Usage: $0 <package_name> --mode <validate|generate>"
     exit 1
 }
 mode="generate"
 
+
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <package-name> --mode=[generate|validate]"
+  exit 1
+fi
+
+PACKAGE_NAME="$1"
+MODE="$2"
+
+# Extract the mode value
+if [[ "$MODE" =~ --mode[=\ ]?(generate|validate) ]]; then
+  MODE="${BASH_REMATCH[1]}"
+else
+  echo "Invalid mode. Use --mode generate or --mode validate"
+  exit 1
+fi
+
 # Fetch the dependencies in JSON format
 deps=$(flutter pub deps --json)
 
-# Parse command-line arguments
-if [[ $# -eq 0 ]]; then
-  echo "Error: No arguments provided."
-  usage
-fi
+PACKAGE_INFO=$(echo "$deps" | jq --arg PACKAGE_NAME "$PACKAGE_NAME" '
+  .packages
+  | map(select(.name == $PACKAGE_NAME))
+  | first
+  | {version: .version, directDependencies: .directDependencies}
+')
 
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --mode)
-      if [[ -n "$2" ]]; then
-        mode="$2"
-        shift 2
-      else
-        echo "Error: --mode requires a value."
-        usage
-      fi
-      ;;
-    *)
-      usage
-      ;;
-  esac
-done
+# Extract directDependencies array from PACKAGE_INFO
+DIRECT_DEPS=$(echo "$PACKAGE_INFO" | jq '.directDependencies')
 
-# Extract the archive URLs for direct dependencies, excluding the package named 'flutter'
-package_info=$(echo "$deps" | jq -r '.packages[] | select(.kind != "root" and .name != "flutter") | "\(.name) \(.version)"')
-sdk_version=$(echo "$deps" | jq -r '.packages[] | select(.kind == "root") | .version')
+# Find all packages whose names are in directDependencies
+MATCHING_PACKAGES=$(echo "$deps" | jq -s --argjson deps "$DIRECT_DEPS" '
+  .[0].packages
+  | map(select(.name as $n | $deps | index($n)))
+  | map(select(.name != "flutter" and (.name | startswith("mapbox_maps_flutter") | not)))
+  | map({name, version})
+')
+
+PACKAGE_VERSION=$(echo "$PACKAGE_INFO" | jq -r '.version')
 
 deps_licenses=""
 # Loop through each name/version pair
 while read -r name version; do
-  
   package_metadata=$(curl -s "https://pub.dev/api/packages/$name/versions/$version")
   archive_url=$(echo "$package_metadata" | jq -r '.archive_url')
   repository_url=$(echo "$package_metadata" | jq -r '.pubspec.repository // "No repository URL found"')
@@ -58,18 +68,20 @@ while read -r name version; do
     license_entry="### $name, $repository_url"$'\n\n'"\`\`\`"$'\n'"$(cat "$license_file")"$'\n'"\`\`\`"$'\n\n'
     deps_licenses+="$license_entry"
   else
-    dep_licenses+="No license file found for $name $version"
+    deps_licenses+="No license file found for $name $version"
   fi
   # Add your logic here to handle each package
-done <<< "$package_info"
+done < <(echo "$MATCHING_PACKAGES" | jq -r '.[] | "\(.name) \(.version)"')
 
-mobile_dir='mapbox_maps_flutter_mobile'
+if [[ "$PACKAGE_NAME" == "mapbox_maps_flutter" ]]; then
+  mobile_dir="mapbox_maps_flutter_mobile"
+else
+  mobile_dir="$PACKAGE_NAME"
+fi
 
 ios_sdk_version=$(grep -A 1 'mapbox-maps-ios' $mobile_dir/ios/mapbox_maps_flutter/Package.swift | grep 'exact' | sed -E 's/.*"([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9.]+)?)".*/\1/')
 android_sdk_version=$(grep 'com.mapbox.maps:android' $mobile_dir/android/build.gradle | sed -E 's/.*"com\.mapbox\.maps:android:([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9.]+)?)".*/\1/')
 
-echo "iOS SDK version: $ios_sdk_version"
-echo "Android SDK version: $android_sdk_version"
 # Fetch the license files for iOS and Android SDKs
 ios_license_content=$(curl -s "https://raw.githubusercontent.com/mapbox/mapbox-maps-ios/v$ios_sdk_version/LICENSE.md")
 android_license_content=$(curl -s "https://raw.githubusercontent.com/mapbox/mapbox-maps-android/v$android_sdk_version/LICENSE.md")
@@ -94,22 +106,22 @@ __IOS_LICENSES__
 
 __ANDROID_LICENSES__
 "
-license=$(echo "$license_template" | m4 -D __SDK_VERSION__="$sdk_version" \
+license=$(echo "$license_template" | m4 -D __SDK_VERSION__="$PACKAGE_VERSION" \
                                         -D __DEPS_LICENSES__="$deps_licenses" \
                                         -D __IOS_LICENSES__="$ios_license_content" \
                                         -D __ANDROID_LICENSES__="$android_license_content" \
                                         -D __YEAR__="$(date +%Y)")
 
-if [ "$mode" == "validate" ]; then
-    if [[ "$(cat LICENSE)" == "$license" ]]; then
+if [ "$MODE" == "validate" ]; then
+    if [[ "$(cat $mobile_dir/LICENSE)" == "$license" ]]; then
         echo "License file is up-to-date."
         exit 0
     else
         echo "⚠️ License is not up-to-date. ⚠️"
         exit 1
     fi
-elif [ "$mode" == "generate" ]; then
-    echo "$license" > LICENSE
+elif [ "$MODE" == "generate" ]; then
+    echo "$license" > $mobile_dir/LICENSE
     echo "License file has been updated." >&2
 else
     usage
