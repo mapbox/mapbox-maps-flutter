5060726c5ca5c314deca18090026e87d77e664a2
Mai Mai <mai.mai@mapbox.com>
2026-03-04 13:12:01 +0200

    create mapbox_maps_flutter_platform_interface - update to use min dart 3.11
    GitOrigin-RevId: cc6aef109a17a2095125be313537d81096ea6a63

--- example/lib/example.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract interface class Example extends Widget {
-  Widget get leading;
-  String get title;
-  String? get subtitle;
-}
--- lib/src/package_info.dart
+++
@@ -1,4 +0,0 @@
-part of mapbox_maps_flutter;
-
-const String mapboxPluginVersion = '2.25.0-SNAPSHOT-06-05--02-05.git-c8e5654';
-
