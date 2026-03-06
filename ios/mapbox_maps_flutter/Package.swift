7ea5e0fe6321ed94fcc93cf3d225d1680addb929
mapbox-ci <no-reply@mapbox.com>
2026-03-06 16:07:15 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-03-06--15-09.git-c08be41`
    GitOrigin-RevId: 9920b68723f77c8927cd9d296edc999ba5303b29

--- ios/mapbox_maps_flutter/Package.swift
+++ ios/mapbox_maps_flutter/Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapboxMapsVersion: Version = "11.21.0"
+let mapboxMapsVersion: Version = "11.21.0-SNAPSHOT-03-06--15-09.git-c08be41"
 
 let mapboxMapsPackage = mapboxMapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
--- lib/src/package_info.dart
+++ lib/src/package_info.dart
@@ -1,4 +1,4 @@
 part of mapbox_maps_flutter;
 
-const String mapboxPluginVersion = '2.20.0-SNAPSHOT-02-26--04-58.git-514e5b0';
+const String mapboxPluginVersion = '2.20.0-SNAPSHOT-03-06--15-09.git-c08be41';
 
