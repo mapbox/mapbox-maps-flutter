81a203500283fd6d73891a0a14ae6167eae640fc
Artem Panfilov <artem.panfilov@mapbox.com>
2026-03-19 21:34:58 +0200

    Bump version
    GitOrigin-RevId: db0e5d68453b338dbf314d08397fecb54ec3e092

--- ios/mapbox_maps_flutter/Package.swift
+++ ios/mapbox_maps_flutter/Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapboxMapsVersion: Version = "11.21.0-SNAPSHOT-03-19--17-21.git-256ddf1"
+let mapboxMapsVersion: Version = "11.21.0-alpha.1"
 
 let mapboxMapsPackage = mapboxMapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
