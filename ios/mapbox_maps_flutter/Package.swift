6bb04ae7127ccfd9cf34f0f2d0539a914e1b27c0
Fedor Tolstonozhenko <fedor.tolstonozhenko@mapbox.com>
2026-03-23 15:29:25 +0100

    [skip ci] Bump release version to v0.21.0-rc.1
    GitOrigin-RevId: 0116def830de3d143a23707b7c698015d1023d73

--- ios/mapbox_maps_flutter/Package.swift
+++ ios/mapbox_maps_flutter/Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapboxMapsVersion: Version = "11.22.0"
+let mapboxMapsVersion: Version = "11.21.0-rc.1"
 
 let mapboxMapsPackage = mapboxMapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
