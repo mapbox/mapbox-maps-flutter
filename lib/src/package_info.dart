bb3bd6a3d8d4c4f6674ba03b90deda5db9c11ac9
mapbox-ci <no-reply@mapbox.com>
2026-04-08 09:39:18 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-04-08--02-04.git-ccd3bc5`
    GitOrigin-RevId: 769b8590e1584bf8522e8ad6ba8f83f49e93aca8

--- lib/src/package_info.dart
+++ lib/src/package_info.dart
@@ -1,4 +1,4 @@
 part of mapbox_maps_flutter;
 
-const String mapboxPluginVersion = '2.22.0-SNAPSHOT-04-07--10-38.git-0fc6806';
+const String mapboxPluginVersion = '2.22.0-SNAPSHOT-04-08--02-04.git-ccd3bc5';
 
