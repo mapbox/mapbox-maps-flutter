mapbox/mapbox-maps-ios

[skip ci][no ci] Bump snapshot versions `SNAPSHOT-05-22--02-05.git-63c0f81`
GitOrigin-RevId: 4f5c99042648ed4e1ff2d69cbfbaa2dea49e9b2c

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
-let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
+let commonVersion: Version = "24.25.0-SNAPSHOT-05-22--02-05.git-63c0f81"
+let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-22--02-05.git-63c0f81"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

mapbox/mapbox-maps-flutter

ios/mapbox_maps_flutter/Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapboxMapsVersion: Version = "11.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
+let mapboxMapsVersion: Version = "11.25.0-SNAPSHOT-05-22--02-05.git-63c0f81"
 
 let mapboxMapsPackage = mapboxMapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"