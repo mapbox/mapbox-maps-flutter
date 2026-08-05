9f8f4df07e9bb68b9ff0a93a77d3d650d6c00727
Viktor Kononov <viktor.kononov@mapbox.com>
2026-08-05 13:13:28 +0200

    sync version alpha.2
    GitOrigin-RevId: e04287fd4ce483433ae9092347322dc8b848c399

--- ios/mapbox_maps_flutter/Package.swift
+++ ios/mapbox_maps_flutter/Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let mapboxMapsVersion: Version = "11.29.0-alpha.1"
+let mapboxMapsVersion: Version = "11.29.0-alpha.2"
 
 let mapboxMapsPackage = mapboxMapsVersion.description.contains("SNAPSHOT")
     ? "mapbox-maps-ios-binary"
